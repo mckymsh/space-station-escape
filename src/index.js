cc9421ceecf30d820015b8cfe1360aaf7b5b7586
Mickey Nash <michaelpeternash@gmail.com>
2021-02-19 13:38:43 -0500

    Starry background exists but is clumsy

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './index.css'; 
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+	<div id="stars" className="stars"></div>
+	<div id="stars2" className="stars"></div>
+	<div id="stars3" className="stars"></div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
